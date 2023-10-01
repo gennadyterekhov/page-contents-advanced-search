e79195208223915ec07d261c2b30a9f04a3aa405
Gennady Terekhov <ter.ge@yandex.ru>
2023-10-01 20:41:36 +0300

    d

---
+++ test/searcher.js
@@ -0,0 +1,42 @@
+class Searcher {
+
+    searchCaseInsensitive(haystack, needle) {
+        const lowerHaystack = haystack.toLowerCase();
+        const lowerNeedle = needle.toLowerCase();
+        const indices = [];
+        let index = -1;
+
+        while ((index = lowerHaystack.indexOf(lowerNeedle, index + 1)) !== -1) {
+            indices.push(index);
+        }
+
+        return indices;
+    }
+
+    searchCaseSensitive(haystack, needle) {
+        const indices = [];
+        let index = -1;
+
+        while ((index = haystack.indexOf(needle, index + 1)) !== -1) {
+            indices.push(index);
+        }
+
+        return indices;
+    }
+
+    searchRegex(haystack, regex) {
+        const indices = [];
+        let match;
+        const re = new RegExp(regex, 'g');
+
+        while ((match = re.exec(haystack)) !== null) {
+            indices.push(match.index);
+        }
+
+        return indices;
+    }
+}
+
+
+
+
