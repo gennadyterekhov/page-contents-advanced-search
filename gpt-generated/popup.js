49975c1430d09cc544830520be87e19a87ac7eb0
Gennady Terekhov <ter.ge@yandex.ru>
2023-10-01 20:57:05 +0300

    d

--- gpt-generated/popup.js
+++ gpt-generated/popup.js
@@ -5,20 +5,22 @@ const acrossLinksCheckbox = document.getElementById('across-links-only-checkbox'
 const searchInput = document.getElementById('search-query-input');
 const searchButton = document.getElementById('search-button');
 const resultsDiv = document.getElementById('search-results');
+const messageDiv = document.getElementById('message');
 let linksText = '';
-let pageContentsAsText = '';
 let pageContentsAsDocument = null;
 let linksExist = true;
 
 let isLoading = false;
 
+const searcher = new Searcher();
+const presenter = new Presenter(resultsDiv);
 
-// Назначаем обработчики событий
+searchButton.addEventListener('click', handleSearchButtonClick);
+chrome.runtime.onMessage.addListener(onMessageListener);
 caseInsensitiveCheckbox.addEventListener('change', toggleCaseInsensitive);
 regexCheckbox.addEventListener('change', toggleRegex);
 searchButton.addEventListener('click', handleSearchButtonClick);
 
-// Функции изменения состояния чекбоксов
 function toggleCaseInsensitive() {
     caseInsensitiveCheckbox.checked = !caseInsensitiveCheckbox.checked;
     if (caseInsensitiveCheckbox.checked) {
@@ -37,7 +39,6 @@ function toggleAcrossLinks() {
     acrossLinksCheckbox.checked = !acrossLinksCheckbox.checked;
 }
 
-// Функция получения массива всех ссылок на текущей странице
 function getAllLinks() {
     const links = document.getElementsByTagName('a');
     const linkArray = Array.from(links);
@@ -65,132 +66,29 @@ function getLinksText() {
     return linksText;
 }
 
-// Функция поиска с учетом регистра
-function searchCaseSensitive(haystack, needle) {
-    const indices = [];
-    let index = -1;
 
-    while ((index = haystack.indexOf(needle, index + 1)) !== -1) {
-        indices.push(index);
-    }
 
-    return indices;
-}
-
-// Функция поиска без учета регистра
-function searchCaseInsensitive(haystack, needle) {
-    const lowerHaystack = haystack.toLowerCase();
-    const lowerNeedle = needle.toLowerCase();
-    const indices = [];
-    let index = -1;
-
-    while ((index = lowerHaystack.indexOf(lowerNeedle, index + 1)) !== -1) {
-        indices.push(index);
-    }
-
-    return indices;
-}
-
-// Функция поиска по регулярному выражению
-function searchRegex(haystack, regex) {
-    const indices = [];
-    let match;
-    const re = new RegExp(regex, 'g');
-
-    while ((match = re.exec(haystack)) !== null) {
-        indices.push(match.index);
-    }
-
-    return indices;
-}
-
-// Функция отображения результатов
-function showResults(indices, originalText) {
-    const resultsHTML = getHtmlResults(indices, originalText);
-    resultsDiv.innerHTML = resultsHTML;
-}
-
-// Функция получения HTML-разметки для результатов
-function getHtmlResults(indices, originalText) {
-    let resultHtml = '';
-    const contextLength = 20;
-
-    for (const index of indices) {
-        const startIndex = Math.max(0, index - contextLength);
-        const endIndex = Math.min(index + contextLength, originalText.length);
-        const context = '...' + originalText.substring(startIndex, endIndex) + '...';
-        const highlightedContext = context.replace(new RegExp(searchInput.value, 'g'), '<span class="highlight">$&</span>');
-        resultHtml += highlightedContext + '<br>';
-    }
-
-    return resultHtml;
-}
-
-// Обработчик нажатия на кнопку поиска
 async function handleSearchButtonClick() {
     console.log('handleSearchButtonClick');
-
-    const caseInsensitive = caseInsensitiveCheckbox.checked;
-    const regex = regexCheckbox.checked;
-    const acrossLinks = acrossLinksCheckbox.checked;
-    const searchText = searchInput.value;
-    let indices = [];
     let pageText;
-    if (acrossLinks) {
-        const linksText = getLinksText();
-        if (!linksText) {
-            resultsDiv.innerHTML = '';
-            return;
-        }
-        indices = searchCaseInsensitive(linksText, searchText);
-    } else {
-        pageText = await getPageContentsAsText();
-        console.log('pageText', pageText);
 
-        if (!pageText) {
-            resultsDiv.innerHTML = '';
-            return;
-        }
-
-        if (regex) {
-            indices = searchRegex(pageText, searchText);
-        } else if (caseInsensitive) {
-            indices = searchCaseInsensitive(pageText, searchText);
-        } else {
-            indices = searchCaseSensitive(pageText, searchText);
-        }
-    }
-
-    showResults(indices, pageText);
-}
-
-
-// below is human generated code
-
-async function getPageContentsAsText() {
-    console.log('getPageContentsAsText');
-
-    if (pageContentsAsText !== '') {
-        return pageContentsAsText;
-    }
-    const doc = await getPageContentsAsDocument();
-    // pageContentsAsText = doc.body.innerText;
-    //document.all[0].outerHTML
-    pageContentsAsText = doc.all[0].outerHTML;
-    console.log('pageContentsAsText', pageContentsAsText);
-
-    return pageContentsAsText;
+    pageText = await getPageContentsAsDocument();
 }
 
 async function getPageContentsAsDocument() {
+    messageDiv.innerHTML = 'loading';
+
     console.log('getPageContentsAsDocument');
 
     isLoading = true;
     if (pageContentsAsDocument !== null) {
         return pageContentsAsDocument;
     }
-    const response = await chrome.runtime.sendMessage({ action: "getActiveTabContent" });
-    console.log(response);
+    const response = await chrome.runtime.sendMessage({ action: "getActiveTabDocument" });
+    console.log('response', response);
+    if (!response) {
+        return null;
+    }
     pageContentsAsDocument = response.content;
     console.log('pageContentsAsDocument', pageContentsAsDocument);
 
@@ -198,21 +96,32 @@ async function getPageContentsAsDocument() {
     return response;
 }
 
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        console.log('onMessage in popup.js', request);
+async function onMessageListener(request, sender, sendResponse) {
+    if (request.action === 'sendUpdatePopupContentToPopup') {
+        return await sendUpdatePopupContentToPopupListener(request, sender, sendResponse)
 
-        console.log(sender.tab ?
-            "from a content script:" + sender.tab.url :
-            "from the extension");
-        if (request.action === "sendUpdatePopupContentToPopup") {
-            console.log('getting sendUpdatePopupContentToPopup in popup.js');
-
-            console.log('getting page in popup.js', request.content);
-            pageContentsAsDocument = request.content
-            sendResponse({ status: "ok" });
-            return;
-        }
-        sendResponse({ status: "ko", content: 'unknown action' });
     }
-);
+    sendResponse({ status: "ko", content: 'unknown action' });
+}
+
+function sendUpdatePopupContentToPopupListener(request, sender, sendResponse) {
+    console.log('sendUpdatePopupContentToPopupListener popup.js', request);
+
+    pageContentsAsDocument = request.content;
+    messageDiv.innerHTML = '';
+    sendResponse({ status: "ok", content: 'got doc text' });
+
+    continueSearch();
+}
+
+function continueSearch() {
+    const searchText = searchInput.value;
+
+    if (!pageContentsAsDocument) {
+        resultsDiv.innerHTML = ' что-то не так, не нашел текст на странице';
+        return;
+    }
+    indices = searcher.searchCaseInsensitive(pageContentsAsDocument, searchText);
+
+    presenter.showResults(indices, pageContentsAsDocument);
+}
