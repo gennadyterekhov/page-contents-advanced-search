8f38e32079d22481e42eb9bff752a1eaf2271742
Gennady Terekhov <ter.ge@yandex.ru>
2023-10-01 20:34:51 +0300

    h

--- test/content.js
+++ test/content.js
@@ -1,20 +1,10 @@
-async function sendUpdatePopupContent() {
-    console.log('sendUpdatePopupContent content.js', document);
-    const response = await chrome.runtime.sendMessage({ action: "updatePopupContent", content: document });
-    console.log(response);
-}
-
 async function onMessageListener(request, sender, sendResponse) {
     console.log('onMessageListener content.js');
 
-    console.log(sender.tab ?
-        "from a content script:" + sender.tab.url :
-        "from the extension");
     if (request.action === "getActiveTabDocument") {
         console.log('getting page in content.js , sending document');
         console.log('document', document);
 
-        // await sendUpdatePopupContent();
         sendResponse({ status: "ok", content: document.body.innerHTML });
         return;
     }
