d5a2b74a194deeea5694f556eef4ea6baa1dd5ac
Gennady Terekhov <ter.ge@yandex.ru>
2023-10-03 21:48:16 +0300

    d

--- gpt-generated/content.js
+++ gpt-generated/content.js
@@ -1,19 +1,9 @@
 async function onMessageListener(request, sender, sendResponse) {
-    console.log('onMessageListener content.js');
-
     if (request.action === "getActiveTabDocument") {
-        console.log('getting page in content.js , sending document');
-        console.log('document', document);
-
-        sendResponse({ status: "ok", content: document.body.innerHTML });
-        return;
+        return sendResponse({ status: "ok", content: document.body.innerText });
     }
     sendResponse({ status: "ko", content: 'unknown action' });
 }
 
 
 chrome.runtime.onMessage.addListener(onMessageListener);
-
-console.log('raw root content.js document', document)
-console.log('raw root content.js document.textContent', document.textContent)
-
