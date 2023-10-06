53a14a5580bb75b271fc70914c473e4113353c0b
Gennady Terekhov <ter.ge@yandex.ru>
2023-10-07 00:41:28 +0300

    ds

---
+++ gpt-generated/data-structure.js
@@ -0,0 +1,16 @@
+class DataStructure {
+    constructor(document) {
+        this.caseInsensitiveCheckbox = document.getElementById('case-insensitive-checkbox');
+        this.regexCheckbox = document.getElementById('treat-as-regex-checkbox');
+        this.acrossLinksCheckbox = document.getElementById('across-links-only-checkbox');
+        this.searchInput = document.getElementById('search-query-input');
+        this.searchButton = document.getElementById('search-button');
+        this.resultsDiv = document.getElementById('search-results');
+        this.messageDiv = document.getElementById('message');
+        this.linksText = '';
+        this.pageContentsAsDocument = null;
+        this.linksExist = true;
+        this.isLoading = false;
+        this.foundSubstringStartIndices = [];
+    }
+}
