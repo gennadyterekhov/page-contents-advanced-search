3b1efd0d838bf32b5c8612fb549b2efffbe114af
Gennady Terekhov <ter.ge@yandex.ru>
2023-10-07 00:41:33 +0300

    ext

--- gpt-generated/extension.js
+++ gpt-generated/extension.js
@@ -1,15 +1,27 @@
 class Extension {
     constructor() {
-        this.popup = new Popup();
+        this.dataStructure = new DataStructure(document);
+        this.popup = new Popup(this.dataStructure, this.handleSearchButtonClick);
         this.searcher = new Searcher();
-        this.presenter = new Presenter();
-        this.popup = new Popup();
+        this.presenter = new Presenter(this.dataStructure.resultsDiv);
+    }
 
+    startApp() {
+        console.log('app started');
+    }
+
+    async handleSearchButtonClick() {
+        if (!this.dataStructure.pageContentsAsDocument) {
+            this.dataStructure.pageContentsAsDocument = await this.popup.getPageContentsAsDocument();
+        }
+        this.dataStructure.foundSubstringStartIndices = this.searcher.startSearch(
+            this.dataStructure.searchInput.value,
+            this.dataStructure.pageContentsAsDocument
+        );
+
+        this.presenter.renderResultsOnPage(this.dataStructure.foundSubstringStartIndices);
     }
 }
 
-
-
 let ext = new Extension();
-
-ext.popup.main();
+ext.startApp();
